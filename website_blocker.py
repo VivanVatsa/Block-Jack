ec9947237b6e4d280a8a6139f4c7ecc41b562852
Vivan <vivanvatsa@gmail.com>
2020-09-08 17:17:06 +0530

    Create website_blocker.py

---
+++ website_blocker.py
@@ -0,0 +1,10 @@
+import time
+
+hosts_path = r"C:\Windows\System32\drivers\etc\hosts"
+redirect = "127.0.0.1"
+website_list = ["www.instagram.com", "instagram.com",
+                "https://outlook.live.com/mail/0/inbox", "www.outlook.com"]
+
+while True:
+    print(1)
+    time.sleep(5)
