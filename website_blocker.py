a0e811637969f0ee3c9415c3edcef5e6ded70c38
Vivan <vivanvatsa@gmail.com>
2020-09-08 22:08:25 +0530

    Update website_blocker.py

--- website_blocker.py
+++ website_blocker.py
@@ -1,6 +1,7 @@
 import time
 from datetime import datetime as dt
 
+hosts_temp = "hosts"
 hosts_path = r"C:\Windows\System32\drivers\etc\hosts"
 redirect = "127.0.0.1"
 website_list = ["www.instagram.com", "instagram.com",
@@ -9,6 +10,11 @@ website_list = ["www.instagram.com", "instagram.com",
 while True:
     if dt(dt.now().year, dt.now().month, dt.now().day, 8) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 16):
         print("this is your working hour A##, concentrate dumbo!!")
+        # r+ is used to simultaenously read and then accordingly write onto it.
+        with open(hosts_temp, 'r+') as file:
+            content = file.read()
+            print(content)
+
     else:
         print("yeah you can chill now zuckerrrr")
     # print(1)
