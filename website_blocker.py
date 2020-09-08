b002d9cf0a8e9f175008cd0bc64418c6617ae50a
Vivan <vivanvatsa@gmail.com>
2020-09-08 20:00:51 +0530

    adding comparison datetime charts

--- website_blocker.py
+++ website_blocker.py
@@ -1,4 +1,5 @@
 import time
+from datetime import datetime as dt
 
 hosts_path = r"C:\Windows\System32\drivers\etc\hosts"
 redirect = "127.0.0.1"
@@ -6,5 +7,9 @@ website_list = ["www.instagram.com", "instagram.com",
                 "https://outlook.live.com/mail/0/inbox", "www.outlook.com"]
 
 while True:
-    print(1)
+    if dt(dt.now().year, dt.now().month, dt.now().day, 8) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 16):
+        print("this is your working hour A##, concentrate dumbo!!")
+    else:
+        print("yeah you can chill now zuckerrrr")
+    # print(1)
     time.sleep(5)
